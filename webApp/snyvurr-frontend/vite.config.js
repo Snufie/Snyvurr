00344ac4cecff1a2265fd52bd707e7323dfa4c2d
Snufie <127324@leerling.k-s-e.nl>
2023-09-25 17:54:54 +0200

    now gonna use PolyFills

--- webApp/snyvurr-frontend/vite.config.js
+++ webApp/snyvurr-frontend/vite.config.js
@@ -1,11 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src/main.js', import.meta.url))
