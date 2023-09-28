8807d55e055d982c2192431df7754306665ce01d
Snufie <127324@leerling.k-s-e.nl>
2023-09-28 12:10:40 +0200

    fix import error

--- snyvurr-frontend/vite.config.js
+++ snyvurr-frontend/vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import Vue from '@vitejs/plugin-vue'
-import VueMacros from 'unplugin-vue-macros/vite'
+import VueMacros from 'unplugin-vue-macros'
 
 // https://vitejs.dev/config/
 export default defineConfig({
