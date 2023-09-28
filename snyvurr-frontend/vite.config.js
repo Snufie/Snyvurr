906135c4e505c2307d102df4ab8d05b953de1b67
Snufie <127324@leerling.k-s-e.nl>
2023-09-28 12:16:02 +0200

    darn

--- snyvurr-frontend/vite.config.js
+++ snyvurr-frontend/vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import Vue from '@vitejs/plugin-vue'
-import VueMacros from 'unplugin-vue-macros'
+import VueMacros from 'unplugin-vue-macros/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
